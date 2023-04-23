816a4311485ee8ea961f178fa0944aad66691b5b
rax02 <0001rkm@gmail.com>
2023-04-23 19:42:04 +0530

    testing autobuild

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("hi im in jenkin");
+        System.out.println("testing auto build with github. update");
     }
 }
